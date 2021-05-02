df80e81b4a537108713c33d84ef858e01f5ee8f3
Brian Chen <brianchen8990@gmail.com>
2021-05-02 22:36:11 +0200

    enable lsp code action keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -29,7 +29,7 @@ map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap=true, silent=t
 map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { noremap=true, silent=true })
 map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap=true, silent=true })
 map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap=true, silent=true })
--- map('n', 'ca', ':Lspsaga code_action<CR>', { noremap=true, silent=true })
+map('n', 'ca', ':Lspsaga code_action<CR>', { noremap=true, silent=true })
 -- map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap=true, silent=true })
 map('n', '<C-p>', ':Lspsaga diagnostic_jump_prev<CR>', { noremap=true, silent=true })
 map('n', '<C-n>', ':Lspsaga diagnostic_jump_next<CR>', { noremap=true, silent=true })
