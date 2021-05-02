09d5979edda09d6f58772f69910508d567de49b3
Brian Chen <brianchen8990@gmail.com>
2021-05-02 22:34:55 +0200

    Add auto_formatters

--- lua/auto.lua
+++ lua/auto.lua
@@ -12,8 +12,32 @@ local function augroups(definitions)
 	end
 end
 
+local auto_formatters = {}
+
+local python_autoformat = {'BufWritePre', '*.py', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'}
+table.insert(auto_formatters, python_autoformat)
+
+local javascript_autoformat = {'BufWritePre', '*.js', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'}
+local javascriptreact_autoformat = {'BufWritePre', '*.jsx', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'}
+local typescript_autoformat = {'BufWritePre', '*.ts', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'}
+local typescriptreact_autoformat = {'BufWritePre', '*.tsx', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'}
+table.insert(auto_formatters, javascript_autoformat)
+table.insert(auto_formatters, javascriptreact_autoformat)
+table.insert(auto_formatters, typescript_autoformat)
+table.insert(auto_formatters, typescriptreact_autoformat)
+
+local lua_format = {'BufWritePre', '*.lua', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'}
+table.insert(auto_formatters, lua_format)
+
+local json_format = {'BufWritePre', '*.json', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'}
+table.insert(auto_formatters, json_format)
+
+local ruby_format = {'BufWritePre', '*.rb', 'lua vim.lsp.buf.formatting_sync(nil,1000)'}
+table.insert(auto_formatters, ruby_format)
+
 augroups({
 	_global = {
+        {'TextYankPost', '*', 'lua require(\'vim.highlight\').on_yank({higroup = \'Search\', timeout = 200})'},
 		{'BufWritePre', '*', ':call TrimWhitespace()'},
         {'BufWinEnter', '*', 'setlocal formatoptions-=c formatoptions-=r formatoptions-=o'},
         {'BufRead', '*', 'setlocal formatoptions-=c formatoptions-=r formatoptions-=o'},
@@ -31,6 +55,15 @@ augroups({
     },
     _markdown = {
         {'FileType', 'markdown', 'setlocal spell foldexpr=MarkdownLevel() foldmethod=expr nofoldenable'}
-    }
+    },
+    _solidity = {
+        {'BufWinEnter', '.sol', 'setlocal filetype=solidity'}, {'BufRead', '*.sol', 'setlocal filetype=solidity'},
+        {'BufNewFile', '*.sol', 'setlocal filetype=solidity'}
+    },
+    _gemini = {
+        {'BufWinEnter', '.gmi', 'setlocal filetype=markdown'}, {'BufRead', '*.gmi', 'setlocal filetype=markdown'},
+        {'BufNewFile', '*.gmi', 'setlocal filetype=markdown'}
+    },
+    _auto_formatters = auto_formatters
 })
 
