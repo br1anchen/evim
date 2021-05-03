0a294c8eba08a9fd9bae364a9ea6095e4cb859f5
Brian Chen <brianchen8990@gmail.com>
2021-05-03 22:54:10 +0200

    Fix lazygit toggleterm config to bind q key as close terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,35 +1,45 @@
-require("toggleterm").setup{
-  size = Term.size,
-  -- open_mapping = [[<c-\>]],
-  hide_numbers = true, -- hide the number column in toggleterm buffers
-  shade_filetypes = {},
-  shade_terminals = Term.shade,
-  shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-  start_in_insert = true,
-  persist_size = true,
-  direction = Term.direction,
-  close_on_exit = true, -- close the terminal window when the process exits
-  shell = Term.shell,
-  -- This field is only relevant if direction is set to 'float'
-  float_opts = {
-    -- The border key is *almost* the same as 'nvim_win_open'
-    -- see :h nvim_win_open for details on borders however
-    -- the 'curved' border is a custom border type
-    -- not natively supported but implemented in this plugin.
-    border =  Term.floatBorder,
-    -- width = <value>,
-    -- height = <value>,
-    winblend = 3,
-    highlights = {
-      border = "Normal",
-      background = "Normal",
+require("toggleterm").setup {
+    size = Term.size,
+    -- open_mapping = [[<c-\>]],
+    hide_numbers = true, -- hide the number column in toggleterm buffers
+    shade_filetypes = {},
+    shade_terminals = Term.shade,
+    shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+    start_in_insert = true,
+    persist_size = true,
+    direction = Term.direction,
+    close_on_exit = true, -- close the terminal window when the process exits
+    shell = Term.shell,
+    -- This field is only relevant if direction is set to 'float'
+    float_opts = {
+        -- The border key is *almost* the same as 'nvim_win_open'
+        -- see :h nvim_win_open for details on borders however
+        -- the 'curved' border is a custom border type
+        -- not natively supported but implemented in this plugin.
+        border = Term.floatBorder,
+        -- width = <value>,
+        -- height = <value>,
+        winblend = 3,
+        highlights = {border = "Normal", background = "Normal"}
     }
-  }
 }
 
-local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", direction = "float", hidden = true })
+local Terminal = require('toggleterm.terminal').Terminal
+
+local lazygit = Terminal:new({
+    cmd = "lazygit",
+    direction = "float",
+    float_opts = {border = "double"},
+    -- function to run on opening the terminal
+    on_open = function(term)
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+    end,
+    -- function to run on closing the terminal
+    on_close = function(term)
+        vim.cmd("Closing terminal")
+    end
+})
 
 function lazygit_toggle()
-  lazygit:toggle()
+    lazygit:toggle()
 end
