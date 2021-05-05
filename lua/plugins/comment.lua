6d554484de5c64e08e48dbb0b29ffbed27489f9c
Brian Chen <brianchen8990@gmail.com>
2021-05-05 11:51:18 +0200

    Change nvim_comment config

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,6 +1,12 @@
 require('nvim_comment').setup({
-	marker_padding = true,
-	comment_empty = false,
-    line_mapping = "<C-_>",
-    operator_mapping = "<C-_>"
+    -- Linters prefer comment and line to have a space in between markers
+    marker_padding = true,
+    -- should comment out empty or whitespace only lines
+    comment_empty = false,
+    -- Should key mappings be created
+    create_mappings = true,
+    -- Normal mode mapping left hand side
+    line_mapping = "gcc",
+    -- Visual/Operator mapping left hand side
+    operator_mapping = "gc"
 })
